7cf9e981ab871a1ba99479a6cb3f08c989c72741
SantiagoRomero7 <santiagoinstagram23@gmail.com>
2025-08-22 10:04:30 -0500

    feat: :sparkles: agregar consultas con expresiones regulares para productos y clientes
    agregar consultas con expresiones regulares para productos y clientes

---
+++ scripts/regex.js
@@ -0,0 +1,8 @@
+// 1. Productos cuyo nombre empiece por "Boro"
+db.productos.find({ nombre: { $regex: /^Boro/ } });
+
+// 2. Productos cuyo nombre contenga la palabra "con"
+db.productos.find({ nombre: { $regex: /\bcon\b/i } });
+
+// 3. Clientes cuyo nombre tenga la letra "z" (insensible a mayúsculas/minúsculas)
+db.clientes.find({ nombre: { $regex: /z/i } });
