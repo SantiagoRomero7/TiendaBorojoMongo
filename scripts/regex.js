5f2be448ab5f5f61fcd15928d875597662e73590
Ricardo-Palomino <rp0459510@gmail.com>
2025-08-24 15:02:40 -0500

    fix: :bug: Actualizacion de una consulta
    Actualizacion de una consulta
    
    by: Ricardo P

--- scripts/regex.js
+++ scripts/regex.js
@@ -2,7 +2,7 @@
 db.productos.find({ nombre: { $regex: /^Boro/ } });
 
 // 2. Productos cuyo nombre contenga la palabra "con"
-db.productos.find({ nombre: { $regex: /\bcon\b/i } });
+db.productos.find({ nombre: { $regex: "con", $options: "i" } });
 
 // 3. Clientes cuyo nombre tenga la letra "z" (insensible a mayúsculas/minúsculas)
 db.clientes.find({ nombre: { $regex: /z/i } });
